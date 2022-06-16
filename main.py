2f01ed33beaa9b2628d7e515e02f7e79719794d0
zearakun <91083604+zearakun@users.noreply.github.com>
2022-06-16 10:55:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ with open("config.json","r+") as f:
   url = config["url"]
 
 print(message.get(url))
+
+name = input("name:")
+contnet = input("content:")
+
+data = {
+  "name": name,
+  "content": content
+}
+
+print(message.post(url,data=data))
